0adf926e0735df2232c67d2cc935815affa1d15b
bithiah01 <bithiahcorpuz@gmail.com>
2021-11-18 06:58:44 +0800

    Firstupload

---
+++ FitnessApp/FitnessApp/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FAbrands.otf", Alias = "FAbrands")]
+[assembly: ExportFont("FAregular.otf", Alias = "FAregular")]
+[assembly: ExportFont("FAsolid.otf", Alias = "FAsolid")]
+
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit { }
+}
